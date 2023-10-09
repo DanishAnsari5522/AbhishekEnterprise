f5aa5840224176247b1a1b2260979363cb405105
DanishAnsari5522 <ddanish1928@gmail.com>
2023-10-09 17:59:17 +0530

    Auth

--- pages/admin/MasterKey/addItem/addMaterial.tsx
+++ pages/admin/MasterKey/addItem/addMaterial.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import AddMaterial from '../../../../components/MasterKey/AddItem/AddMaterial';
-
+import { useRouter } from 'next/router';
 export default function addMaterial() {
+    const router = useRouter()
+    useEffect(() => {
+        let auth1 = localStorage.getItem('user');
+        if (!auth1) {
+            // console.log(JSON.parse(auth1).data['type']);
+            // if (JSON.parse(auth1).data['phone'] !== 9262786676) {
+            //     router.push("/");
+            // }
+            router.push("/");
+        }
+    })
     return (
         <div>
             <AddMaterial />
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -6,11 +6,12 @@ const Admin = () => {
     const router = useRouter()
     useEffect(() => {
         let auth1 = localStorage.getItem('user');
-        if (auth1) {
-            console.log(JSON.parse(auth1).data['type']);
-            if (JSON.parse(auth1).data['phone'] !== 9262786676) {
-                router.push("/");
-            }
+        if (!auth1) {
+            // console.log(JSON.parse(auth1).data['type']);
+            // if (JSON.parse(auth1).data['phone'] !== 9262786676) {
+            //     router.push("/");
+            // }
+            router.push("/");
         }
     })
     return (
